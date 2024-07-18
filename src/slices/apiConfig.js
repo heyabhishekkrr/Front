34bbf3e1e95654e665cdc6f160a77f5a2c58e60d
heyabhishekkrr <76234288+heyabhishekkrr@users.noreply.github.com>
2024-07-19 00:59:13 +0530

    Update apiConfig.js

--- src/slices/apiConfig.js
+++ src/slices/apiConfig.js
@@ -1,2 +1,2 @@
 // Define API_SERVER_URL in a separate file, for example, apiConfig.js
-export const API_SERVER_URL = 'https://finalapi-puce.vercel.app/';
+export const API_SERVER_URL = 'https://finalapi-puce.vercel.app';
