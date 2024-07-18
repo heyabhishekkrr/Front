5305aa9be955a3ded5885971146df2d82df22a3a
heyabhishekkrr <76234288+heyabhishekkrr@users.noreply.github.com>
2024-07-19 00:54:59 +0530

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,11 +1,10 @@
-// Import necessary functions from the toolkit/query package
+// In your userApiSlice.js (or any file where you're using API_SERVER_URL)
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_SERVER_URL } from "./apiConfig"; // Adjust the path as per your file structure
 
-// Define your API server URL and endpoint paths
-const API_SERVER_URL = 'https://finalapi-puce.vercel.app';
+// Use API_SERVER_URL in your slice configuration
 const USERS_URL = `${API_SERVER_URL}/api/users`;
 
-// Create your apiSlice using createApi
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: API_SERVER_URL,
