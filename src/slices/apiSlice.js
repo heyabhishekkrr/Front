5aa78029a61b453eee8374bcf32f47e5e7773133
heyabhishekkrr <76234288+heyabhishekkrr@users.noreply.github.com>
2024-07-19 00:42:26 +0530

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,10 +1,15 @@
+// /vercel/path0/src/slices/userApiSlice.js
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const SERVER_URL = 'https://finalapi-puce.vercel.app'; // Use a different variable name
+// Existing declaration of API_SERVER_URL
+const API_SERVER_URL = 'https://finalapi-puce.vercel.app';
+
+const USERS_URL = `${API_SERVER_URL}/api/users`;
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: SERVER_URL,
+    baseUrl: API_SERVER_URL,
     // Optionally, you can add other default headers, credentials, etc. here
   }),
   tagTypes: ["User"],
@@ -17,5 +22,3 @@ export const apiSlice = createApi({
     // }),
   }),
 });
-
-
