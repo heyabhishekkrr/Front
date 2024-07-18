1b58b2eee6af8fd354407f499b0bf9742297339f
heyabhishekkrr <76234288+heyabhishekkrr@users.noreply.github.com>
2024-07-19 00:22:32 +0530

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,9 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
+  reducerPath: "api", // The reducer path for your API state slice
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
+    baseUrl: "https://finalapi-puce.vercel.app/", // Replace with your API base URL
+    // You can also add headers, credentials, or other fetch options here if needed
   }),
+ 
+
   tagTypes: ["User"],
   // eslint-disable-next-line no-unused-vars
   endpoints: (builder) => ({}),
