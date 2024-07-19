cb6ac1e21f366d3dfa33dc0b84d9f1a394b87ba7
heyabhishekkrr <76234288+heyabhishekkrr@users.noreply.github.com>
2024-07-19 12:47:37 +0530

    v3

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,21 +1,10 @@
-// In your userApiSlice.js (or any file where you're using API_SERVER_URL)
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { API_SERVER_URL } from "./apiConfig"; // Adjust the path as per your file structure
-
-// Use API_SERVER_URL in your slice configuration
-const USERS_URL = `${API_SERVER_URL}/api/users`;
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: API_SERVER_URL,
-    // Optionally, you can add other default headers, credentials, etc. here
+    baseUrl: "",
   }),
   tagTypes: ["User"],
-  endpoints: (builder) => ({
-    // Define your endpoints here
-    // Example:
-    // getUserById: builder.query({
-    //   query: (userId) => `/users/${userId}`,
-    // }),
-  }),
+  // eslint-disable-next-line no-unused-vars
+  endpoints: (builder) => ({}),
 });
